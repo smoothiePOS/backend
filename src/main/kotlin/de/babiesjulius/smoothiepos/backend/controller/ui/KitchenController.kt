383ea98bffdcbff996b03ab5f0a8e805230bb36c
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 20:19:48 +0200

    - documented KitchenController

--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/ui/KitchenController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/ui/KitchenController.kt
@@ -1,20 +1,40 @@
 package de.babiesjulius.smoothiepos.backend.controller.ui
 
-import com.google.gson.Gson
 import de.babiesjulius.smoothiepos.backend.database.Database
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@Tag(name = "Kitchen", description = "The Kitchen API - used for single kitchen display and product management")
 class KitchenController {
 
-    private data class KitchenOrder(val id: String, val products: List<KitchenOrderDetail>, val date: Long)
-    private data class KitchenOrderDetail(val product: String, val amount: Int)
+    data class KitchenOrder(val id: String, val products: List<KitchenOrderDetail>, val date: Long)
+    data class KitchenOrderDetail(val product: String, val amount: Int)
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200", description = "Returns all orders that are not finished", content = [
+                Content(
+                    mediaType = "application/json",
+                    array = ArraySchema(
+                        arraySchema = Schema(
+                            implementation = KitchenOrder::class
+                        )
+                    )
+                )
+            ]
+        )
+    )
     @GetMapping("/kitchen/orders")
-    fun getOrders(): ResponseEntity<String> {
+    fun getOrders(): ResponseEntity<Array<KitchenOrder>> {
         val database = Database.getDatabase()
         val orders = database.orderTable.filter(listOf(Triple("status", "=", "0"))).toList()
         val kitchenOrders = arrayListOf<KitchenOrder>()
@@ -26,9 +46,21 @@ class KitchenController {
             }
             kitchenOrders.add(KitchenOrder(order.id!!, kitchenOrderDetails, order.date!!))
         }
-        return ResponseEntity.ok().body(Gson().toJson(kitchenOrders))
+        return ResponseEntity.ok().body(kitchenOrders.toTypedArray())
     }
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200", description = "Finishes the order with the given id", content = [
+                Content(
+                    mediaType = "text/plain",
+                    schema = Schema(
+                        implementation = String::class
+                    )
+                )
+            ]
+        )
+    )
     @GetMapping("/kitchen/order/{orderId}/finish")
     fun finishOrder(@PathVariable orderId: String): ResponseEntity<String> {
         val database = Database.getDatabase()
