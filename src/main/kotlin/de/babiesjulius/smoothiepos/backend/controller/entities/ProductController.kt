8d9e9e5b6f8b6b2e65628b9fdaf68630013178a3
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 20:27:00 +0200

    - fixed wrong array type in ProductController

--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
@@ -2,6 +2,9 @@ package de.babiesjulius.smoothiepos.backend.controller.entities
 
 import de.babiesjulius.smoothiepos.backend.database.Database
 import de.babiesjulius.smoothiepos.backend.objects.Product
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
@@ -25,9 +28,9 @@ class ProductController {
     @ApiResponses(
         ApiResponse(
             responseCode = "200", description = "Successfully created product", content = [
-                io.swagger.v3.oas.annotations.media.Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(
+                Content(
+                    mediaType = "text/plain",
+                    schema = Schema(
                         implementation = String::class,
                         description = "ID of the created product",
                         example = "86c3d35e-13d1-4755-9f29-3ab552e109cb"
@@ -35,7 +38,11 @@ class ProductController {
                 )
             ]
         ),
-        ApiResponse(responseCode = "400", description = "Product already exists", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
+        ApiResponse(
+            responseCode = "400",
+            description = "Product already exists",
+            content = [Content(mediaType = "text/plain")]
+        ),
     )
     @PostMapping("/product")
     fun createProduct(product: ProductPostRequest): ResponseEntity<String> {
@@ -59,7 +66,14 @@ class ProductController {
     }
 
     @ApiResponses(
-        ApiResponse(responseCode = "200", description = "Get all products", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Array<Product>::class))]),
+        ApiResponse(
+            responseCode = "200",
+            description = "Get all products",
+            content = [Content(
+                mediaType = "application/json",
+                array = ArraySchema(arraySchema = Schema(implementation = Product::class))
+            )]
+        ),
     )
     @GetMapping("/product", produces = ["application/json"])
     fun getProducts(): ResponseEntity<Array<Product>> {
@@ -67,8 +81,16 @@ class ProductController {
     }
 
     @ApiResponses(
-        ApiResponse(responseCode = "200", description = "Successfully changed availability of product", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
-        ApiResponse(responseCode = "400", description = "Product not found", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully changed availability of product",
+            content = [Content(mediaType = "text/plain")]
+        ),
+        ApiResponse(
+            responseCode = "400",
+            description = "Product not found",
+            content = [Content(mediaType = "text/plain")]
+        ),
     )
     @GetMapping("/product/{id}/availability/{available}")
     fun setProductAvailable(@PathVariable id: String, @PathVariable available: Boolean): ResponseEntity<String> {
