9213ada4a361863e70dbf1508ff448145a593853
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 20:27:23 +0200

    - added tag to ProductController

--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.sql.SQLIntegrityConstraintViolationException
 
 @RestController
+@Tag(name = "Product", description = "The Product API")
 class ProductController {
 
     data class ProductPostRequest(
