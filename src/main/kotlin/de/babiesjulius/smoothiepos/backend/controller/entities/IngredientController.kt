20a0692d76c5563b3cc34eead97172a6736c2f21
Julius Babies <julius@familie-babies.de>
2023-07-04 20:28:22 +0200

    26 build api documentation (#28)
    * - dependency
    
    * - documented ingredient controller
    
    * - documented ingredient controller
    
    * - documented product controller
    
    * - documented product controller
    
    * - documented CashpointController
    
    * - documented KitchenController
    
    * - fixed wrong array type in IngredientController
    
    * - fixed wrong array type in ProductController
    
    * - added tag to ProductController
    
    * - added tag to CashpointController

--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/IngredientController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/IngredientController.kt
@@ -1,39 +1,85 @@
 package de.babiesjulius.smoothiepos.backend.controller.entities
 
-import com.google.gson.Gson
 import de.babiesjulius.smoothiepos.backend.database.Database
 import de.babiesjulius.smoothiepos.backend.objects.Ingredient
-import org.springframework.http.HttpEntity
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RestController
 import java.sql.SQLIntegrityConstraintViolationException
 
-@Controller
+@RestController
+@Tag(name = "Ingredient API", description = "API for managing ingredients")
 class IngredientController {
 
+    data class IngredientPostRequest(val name: String, val available: Boolean = false)
+
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200", description = "Successfully created ingredient", content = [
+                Content(
+                    mediaType = "application/json",
+                    schema = io.swagger.v3.oas.annotations.media.Schema(
+                        implementation = String::class,
+                        description = "ID of the created ingredient",
+                        example = "86c3d35e-13d1-4755-9f29-3ab552e109cb"
+                    )
+                )
+            ]
+        ),
+        ApiResponse(responseCode = "400", description = "Ingredient with this name already exists")
+    )
     @PostMapping("/ingredient")
-    fun createIngredient(httpEntity: HttpEntity<String>): ResponseEntity<String> {
-        val body = httpEntity.body ?: return ResponseEntity.status(400).body("No body provided")
-        val ingredient = Gson().fromJson(body, Ingredient::class.java)
+    fun createIngredient(ingredient: IngredientPostRequest): ResponseEntity<String> {
         return try {
-            ResponseEntity.status(200).body(Database.getDatabase().ingredientTable.create(ingredient))
+            ResponseEntity.status(200).body(
+                Database.getDatabase().ingredientTable.create(
+                    Ingredient(
+                        null,
+                        ingredient.name,
+                        ingredient.available
+                    )
+                )
+            )
         } catch (e: SQLIntegrityConstraintViolationException) {
             ResponseEntity.status(400).body("Ingredient already exists")
         }
     }
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200", description = "Successfully fetched ingredients", content = [
+                Content(
+                    mediaType = "application/json",
+                    array = ArraySchema(
+                        arraySchema = Schema(
+                            implementation = Ingredient::class
+                        )
+                    )
+                )
+            ]
+        )
+    )
     @GetMapping("/ingredient")
-    fun getIngredients(): ResponseEntity<String> {
-        return ResponseEntity.status(200).body(Gson().toJson(Database.getDatabase().ingredientTable.read()))
+    fun getIngredients(): ResponseEntity<Array<Ingredient>> {
+        return ResponseEntity.status(200).body(Database.getDatabase().ingredientTable.read())
     }
 
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Successfully changed availability of ingredient"),
+        ApiResponse(responseCode = "400", description = "Ingredient not found")
+    )
     @GetMapping("/ingredient/{id}/availability/{available}")
     fun setIngredientAvailable(@PathVariable id: String, @PathVariable available: Boolean): ResponseEntity<String> {
-        val ingredient = Database.getDatabase().ingredientTable.find(id) ?: return ResponseEntity.status(400).body("Ingredient not found")
+        val ingredient = Database.getDatabase().ingredientTable.find(id) ?: return ResponseEntity.status(400)
+            .body("Ingredient not found")
         Database.getDatabase().ingredientTable.update(ingredient.copy(available = available))
         return ResponseEntity.status(200).build()
     }
--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/ui/CashpointController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/ui/CashpointController.kt
@@ -5,22 +5,45 @@ import de.babiesjulius.smoothiepos.backend.database.Database
 import de.babiesjulius.smoothiepos.backend.objects.LiveOrder
 import de.babiesjulius.smoothiepos.backend.objects.Order
 import de.babiesjulius.smoothiepos.backend.objects.OrderDetail
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpEntity
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@Tag(name = "Cashpoint", description = "Cashpoint related endpoints")
 class CashpointController {
 
-    private data class CashpointProduct(val id: String, val name: String, val price: Int, val deposit: Int, val available: Boolean)
-    private data class CashpointCashpointsResponseCashpoint(val name: String, val available: Boolean)
-    private data class CashpointCashpointsResponse(val cashpoints: Map<String, CashpointCashpointsResponseCashpoint>)
+    data class CashpointProduct(
+        val id: String,
+        val name: String,
+        val price: Int,
+        val deposit: Int,
+        val available: Boolean
+    )
 
+    data class CashpointCashpointsResponseCashpoint(val name: String, val available: Boolean)
+
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Get all products for cashpoint staff view (Contains only necessary data to products (id, name, price, deposit, availability))",
+            content = [Content(
+                mediaType = "application/json", schema = Schema(
+                    implementation = Array<CashpointProduct>::class
+                )
+            )]
+        )
+    )
     @GetMapping("/cashpoint/products")
-    fun getProducts(): ResponseEntity<String> {
+    fun getProducts(): ResponseEntity<Array<CashpointProduct>> {
         val database = Database.getDatabase()
         val products = database.productTable.read()
         val cashpointProducts = arrayListOf<CashpointProduct>()
@@ -29,38 +52,89 @@ class CashpointController {
             product.ingredients.forEach {
                 filter.add(Triple("ingredient_id", "=", it))
             }
-            cashpointProducts.add(CashpointProduct(product.id!!, product.name, product.price, 100, product.available && database.ingredientTable.read().filter { it.id in product.ingredients }.all { it.available })) // TODO deposit to database
+            cashpointProducts.add(
+                CashpointProduct(
+                    product.id!!,
+                    product.name,
+                    product.price,
+                    100,
+                    product.available && database.ingredientTable.read().filter { it.id in product.ingredients }
+                        .all { it.available })
+            ) // TODO deposit to database
         }
-        return ResponseEntity.ok().body(Gson().toJson(cashpointProducts))
+        return ResponseEntity.ok().body(cashpointProducts.toTypedArray())
     }
 
-    private data class LiveOrderResponse(val productName: String, val amount: Int, val price: Int, val deposit: Int)
+    data class LiveOrderResponse(val productName: String, val amount: Int, val price: Int, val deposit: Int)
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Get current order for customer view (Contains only necessary data to ordered products (productName, amount, price, deposit))",
+            content = [Content(
+                mediaType = "application/json", schema = Schema(
+                    implementation = Array<LiveOrderResponse>::class
+                )
+            )]
+        )
+    )
     @GetMapping("/cashpoint/{cashpointId}/order/rt/customer")
-    fun getCustomerOrders(@PathVariable("cashpointId") cashpointId: String): ResponseEntity<String> {
+    fun getCustomerOrders(@PathVariable("cashpointId") cashpointId: String): ResponseEntity<Array<LiveOrderResponse>> {
         val order = Database.getDatabase().liveOrderTable.filter(listOf(Triple("cashpoint_id", "=", cashpointId)))
         val liveOrderResponse = arrayListOf<LiveOrderResponse>()
         order.forEach { liveOrder ->
             val product = Database.getDatabase().productTable.find(liveOrder.productId)
-            liveOrderResponse.add(LiveOrderResponse(product?.name ?: "Unknown", liveOrder.amount!!, product?.price ?: 0, 100)) // TODO transfer deposit to product
+            liveOrderResponse.add(
+                LiveOrderResponse(
+                    product?.name ?: "Unknown",
+                    liveOrder.amount!!,
+                    product?.price ?: 0,
+                    100
+                )
+            ) // TODO transfer deposit to product
         }
-        return ResponseEntity.ok().body(Gson().toJson(liveOrderResponse.toList()))
+        return ResponseEntity.ok().body(liveOrderResponse.toTypedArray())
     }
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Get all cashpoints",
+            content = [Content(
+                mediaType = "application/json"
+            )]
+        )
+    )
     @GetMapping("/cashpoints")
-    fun getCashpoints(): ResponseEntity<String> {
+    fun getCashpoints(): ResponseEntity<HashMap<String, CashpointCashpointsResponseCashpoint>> {
         val cashpoints = Database.getDatabase().cashpointTable.read()
         val cashpointCashpoints = hashMapOf<String, CashpointCashpointsResponseCashpoint>()
         cashpoints.forEach { cashpoint ->
-            cashpointCashpoints[cashpoint.id!!] = CashpointCashpointsResponseCashpoint(cashpoint.name, cashpoint.available)
+            cashpointCashpoints[cashpoint.id!!] =
+                CashpointCashpointsResponseCashpoint(cashpoint.name, cashpoint.available)
         }
-        return ResponseEntity.ok().body(Gson().toJson(CashpointCashpointsResponse(cashpointCashpoints)))
+        return ResponseEntity.ok().body(cashpointCashpoints)
     }
 
     private data class LiveOrderAdd(val productId: String, val amount: Int?)
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Add product to running order at cashpoint",
+            content = [Content(
+                mediaType = "application/json",
+                schema = Schema(
+                    implementation = String::class
+                )
+            )]
+        )
+    )
     @PostMapping("/cashpoint/{cashpointId}/order/rt")
-    fun addOrder(httpEntity: HttpEntity<String>, @PathVariable("cashpointId") cashpointId: String): ResponseEntity<String> {
+    fun addOrder(
+        httpEntity: HttpEntity<String>,
+        @PathVariable("cashpointId") cashpointId: String
+    ): ResponseEntity<String> {
         val body = httpEntity.body ?: return ResponseEntity.badRequest().body("No body provided")
         val liveOrder = Gson().fromJson(body, LiveOrderAdd::class.java)
         return ResponseEntity.ok().body(
@@ -75,12 +149,36 @@ class CashpointController {
         )
     }
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Clear running order at cashpoint",
+            content = [Content(
+                mediaType = "text/plain",
+                schema = Schema(
+                    implementation = String::class
+                )
+            )]
+        )
+    )
     @GetMapping("/cashpoint/{cashpointId}/order/rt/clear")
     fun clearOrder(@PathVariable("cashpointId") cashpointId: String): ResponseEntity<String> {
         Database.getDatabase().liveOrderTable.clear(listOf(Triple("cashpoint_id", "=", cashpointId)))
-        return ResponseEntity.ok().body("OK")
+        return ResponseEntity.ok().build()
     }
 
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Confirm running order at cashpoint (send to kitchen)",
+            content = [Content(
+                mediaType = "text/plain",
+                schema = Schema(
+                    implementation = String::class
+                )
+            )]
+        )
+    )
     @GetMapping("/cashpoint/{cashpointId}/order/rt/confirm")
     fun confirmOrder(@PathVariable("cashpointId") cashpointId: String): ResponseEntity<String> {
         val database = Database.getDatabase()
@@ -92,6 +190,6 @@ class CashpointController {
         val order = Order(null, detail, cashpointId, null, 0)
         database.orderTable.create(order)
         database.liveOrderTable.clear(listOf(Triple("cashpoint_id", "=", cashpointId)))
-        return ResponseEntity.ok().body("OK")
+        return ResponseEntity.ok().build()
     }
 }
