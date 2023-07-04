d68303bf894a9eaaffa0150410604a8aa7353c37
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 19:49:49 +0200

    - dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
