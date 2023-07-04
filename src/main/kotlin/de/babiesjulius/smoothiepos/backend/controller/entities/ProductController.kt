a61573512f682366c0b5074881d5e3c523e3f3f3
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 19:57:57 +0200

    - documented product controller

--- src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
+++ src/main/kotlin/de/babiesjulius/smoothiepos/backend/controller/entities/ProductController.kt
@@ -1,39 +1,82 @@
 package de.babiesjulius.smoothiepos.backend.controller.entities
 
+import com.fasterxml.jackson.databind.BeanDescription
 import com.google.gson.Gson
 import de.babiesjulius.smoothiepos.backend.database.Database
 import de.babiesjulius.smoothiepos.backend.objects.Product
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpEntity
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RestController
 import java.sql.SQLIntegrityConstraintViolationException
 
-@Controller
+@RestController
 class ProductController {
 
+    data class ProductPostRequest(
+        val name: String,
+        val price: Int,
+        val description: String,
+        val ingredients: List<String>,
+        val available: Boolean = false
+    )
+
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200", description = "Successfully created product", content = [
+                io.swagger.v3.oas.annotations.media.Content(
+                    mediaType = "application/json",
+                    schema = io.swagger.v3.oas.annotations.media.Schema(
+                        implementation = String::class,
+                        description = "ID of the created product",
+                        example = "86c3d35e-13d1-4755-9f29-3ab552e109cb"
+                    )
+                )
+            ]
+        ),
+        ApiResponse(responseCode = "400", description = "Product already exists", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
+    )
     @PostMapping("/product")
-    fun createProduct(httpEntity: HttpEntity<String>): ResponseEntity<String> {
-        val body = httpEntity.body ?: return ResponseEntity.status(400).body("No body provided")
-        val product = Gson().fromJson(body, Product::class.java)
+    fun createProduct(product: ProductPostRequest): ResponseEntity<String> {
         return try {
-            ResponseEntity.status(200).body(Database.getDatabase().productTable.create(product))
+            ResponseEntity.status(200).body(
+                Database.getDatabase().productTable.create(
+                    Product(
+                        null,
+                        product.name,
+                        product.price,
+                        "",
+                        null,
+                        product.ingredients,
+                        product.available
+                    )
+                )
+            )
         } catch (e: SQLIntegrityConstraintViolationException) {
             ResponseEntity.status(400).body("Product ${product.name} already exists")
         }
     }
 
-    @GetMapping("/product")
-    fun getProducts(): ResponseEntity<String> {
-        return ResponseEntity.status(200).body(Gson().toJson(Database.getDatabase().productTable.read()))
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Get all products", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Array<Product>::class))]),
+    )
+    @GetMapping("/product", produces = ["application/json"])
+    fun getProducts(): ResponseEntity<Array<Product>> {
+        return ResponseEntity.status(200).body(Database.getDatabase().productTable.read())
     }
 
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Successfully changed availability of product", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
+        ApiResponse(responseCode = "400", description = "Product not found", content = [io.swagger.v3.oas.annotations.media.Content(mediaType = "text/plain")]),
+    )
     @GetMapping("/product/{id}/availability/{available}")
     fun setProductAvailable(@PathVariable id: String, @PathVariable available: Boolean): ResponseEntity<String> {
-        val product = Database.getDatabase().productTable.find(id) ?: return ResponseEntity.status(400).body("Product not found")
+        val product =
+            Database.getDatabase().productTable.find(id) ?: return ResponseEntity.status(400).body("Product not found")
         Database.getDatabase().productTable.changeAvailability(product.copy(available = available))
         return ResponseEntity.status(200).build()
     }
